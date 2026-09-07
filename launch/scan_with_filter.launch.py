1039bf0eb776ad98b7ca05cb025ac31e42e563da
Alberto Tudela <ajtudela@gmail.com>
2026-09-07 13:30:30 +0200

    Remove old laser filter
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- launch/scan_with_filter.launch.py
+++ launch/scan_with_filter.launch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Copyright (c) 2022 Alberto J. Tudela Roldán
+# Copyright  (c) 2024 Alberto J. Tudela Roldán
+# Copyright (c) 2024 Grupo Avispa, DTE, Universidad de Málaga
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,40 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 """Launches a Sick S300 laser scanner node and a filter node."""
 
 import os
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
-
-import launch.events
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import LifecycleNode
 from launch_ros.actions import Node
-from launch_ros.event_handlers import OnStateTransition
-from launch_ros.events.lifecycle import ChangeState
-
-import lifecycle_msgs.msg
 
 
 def generate_launch_description():
     # Default filenames and where to find them
     sicks300_dir = get_package_share_directory('sicks300_ros2')
+    default_param_file = os.path.join(sicks300_dir, 'params', 'default.yaml')
 
-    # Read the YAML parameters file.
-    default_sicks300_param_file = os.path.join(sicks300_dir, 'params', 'default.yaml')
-
-    # Create the launch configuration variables.
-    sicks300_param_file = LaunchConfiguration(
-        'sicks300_param_file', default=default_sicks300_param_file)
+    # Create the launch configuration variables:
+    namespace = LaunchConfiguration('namespace')
+    params_file = LaunchConfiguration('params_file')
 
     # Map these variables to arguments: can be set from the command line or a default will be used
-    sicks300_param_file_launch_arg = DeclareLaunchArgument(
-        'sicks300_param_file',
-        default_value=default_sicks300_param_file,
+    declare_namespace_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace'
+    )
+
+    params_file_launch_arg = DeclareLaunchArgument(
+        'params_file',
+        default_value=default_param_file,
         description='Full path to the Sicks300 parameter file to use'
     )
 
@@ -59,10 +57,11 @@ def generate_launch_description():
     # Prepare the sicks300_ros2 node.
     sicks300_ros2_node = LifecycleNode(
         package='sicks300_ros2',
-        namespace='',
+        namespace=namespace,
         executable='sicks300_ros2',
         name='laser_front',
-        parameters=[sicks300_param_file],
+        parameters=[params_file],
+        autostart=True,
         emulate_tty=True,
         output='screen',
         arguments=[
@@ -70,13 +69,12 @@ def generate_launch_description():
             '--log-level', ['laser_front:=', LaunchConfiguration('log-level')]]
     )
 
-    # Prepare the filter node.
-    filter_node = Node(
-        package='sicks300_ros2',
+    laser_filter_node = Node(
+        package='laser_filters',
         namespace='',
-        executable='scan_filter',
+        executable='scan_to_scan_filter_chain',
         name='scan_filter',
-        parameters=[sicks300_param_file],
+        parameters=[params_file],
         emulate_tty=True,
         output='screen',
         remappings=[
@@ -86,59 +84,10 @@ def generate_launch_description():
             '--log-level', ['scan_filter:=', LaunchConfiguration('log-level')]]
     )
 
-    # laser_filter_node = Node(
-    #     package='laser_filters',
-    #     namespace='',
-    #     executable='scan_to_scan_filter_chain',
-    #     name='scan_filter',
-    #     parameters=[sicks300_param_file],
-    #     emulate_tty=True,
-    #     output='screen',
-    #     remappings=[
-    #         ('/scan_filtered', '/scan/filtered')],
-    #     arguments=[
-    #         '--ros-args',
-    #         '--log-level', ['scan_filter:=', LaunchConfiguration('log-level')]]
-    # )
-
-    # When the sick node reaches the 'inactive' state, make it take the 'activate' transition.
-    register_event_handler_for_sick_reaches_inactive_state = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node=sicks300_ros2_node,
-            goal_state='inactive',
-            entities=[
-                EmitEvent(event=ChangeState(
-                    lifecycle_node_matcher=launch.events.matches_action(sicks300_ros2_node),
-                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
-                )),
-            ],
-        )
-    )
-
-    # When the sick node reaches the 'active' state, start the filter node.
-    register_event_handler_for_sick_reaches_active_state = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node=sicks300_ros2_node,
-            goal_state='active',
-            entities=[
-                filter_node
-            ],
-        )
-    )
-
-    # Make the sicks300_ros2 node take the 'configure' transition.
-    emit_event_to_request_that_sick_does_configure_transition = EmitEvent(
-        event=ChangeState(
-            lifecycle_node_matcher=launch.events.matches_action(sicks300_ros2_node),
-            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-        )
-    )
-
     return LaunchDescription([
-        sicks300_param_file_launch_arg,
+        declare_namespace_arg,
+        params_file_launch_arg,
         declare_log_level_arg,
-        register_event_handler_for_sick_reaches_inactive_state,
-        register_event_handler_for_sick_reaches_active_state,
         sicks300_ros2_node,
-        emit_event_to_request_that_sick_does_configure_transition,
+        laser_filter_node
     ])
